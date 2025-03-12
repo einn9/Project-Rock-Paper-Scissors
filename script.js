a0a1e4e1cd1fec08552f963dcf7318e371cafcb6
einn9 <kratosmana@gmail.com>
2025-03-12 12:14:37 -0300

    Finish assignement part 1, getting computer's choice

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 console.log("testing");
+
+
+// Getting the computer's choice 
+
+function getComputerChoice(number) {
+    return Math.floor(Math.random() * number); // Multiply a random number and round down to a whole number
+}
+
+randomNumber = getComputerChoice(3);
+
+if (randomNumber === 1)
+    {
+        console.log("rock");
+    }
+else if (randomNumber === 2)
+    {
+        console.log("paper");
+    }
+else
+    {
+        console.log("scissors");
+    }
