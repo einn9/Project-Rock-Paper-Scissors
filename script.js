fa9ca08286ef22f524da525ffc48c70666e921a8
einn9 <kratosmana@gmail.com>
2025-03-12 13:57:05 -0300

    Finish assignement part 3, the logic to play a round

--- script.js
+++ script.js
@@ -7,27 +7,73 @@ function getComputerChoice(number) {
     return Math.floor(Math.random() * number); // Multiply a random number and round down to a whole number
 }
 
-randomNumber = getComputerChoice(3);
+randomComputerChoice = getComputerChoice(3);
 
-if (randomNumber === 1)
+
+if (randomComputerChoice === 1)
     {
+        randomComputerChoice = "computerRock";
         console.log("rock");
     }
-else if (randomNumber === 2)
+else if (randomComputerChoice === 2)
     {
+        randomComputerChoice = "computerPaper";
         console.log("paper");
     }
 else
     {
+        randomComputerChoice = "computerScissors";
         console.log("scissors");
     }
 
-// Getting the human's choice
 
+console.log(randomComputerChoice);
+
+
+// Getting user's choice
 
 function getHumanChoice() {
     return prompt("Pick betwen rock, paper, scissors: ");
 }
+    
+let HumanChoice = getHumanChoice();
 
-console.log(getHumanChoice());
 
+// Logic to play a round 
+
+if (HumanChoice === "rock" && randomComputerChoice === "computerRock")
+    {
+    console.log("It's a tie!");
+    }
+else if (HumanChoice === "rock" && randomComputerChoice === "computerPaper")
+    {
+    console.log("You lost!");
+    }
+else if (HumanChoice === "rock" && randomComputerChoice === "computerScissors")
+    {
+    console.log("You Won!");
+    }
+else if (HumanChoice === "paper" && randomComputerChoice === "computerRock")
+    {
+    console.log("You Won!");
+    }
+else if (HumanChoice === "paper" && randomComputerChoice === "computerPaper")
+    {
+    console.log("It's a tie!");
+    }
+else if (HumanChoice === "paper" && randomComputerChoice === "computerScissors")
+    {
+    console.log("You lost!");
+    }
+else if (HumanChoice === "scissors" && randomComputerChoice === "computerRock")
+    {
+    console.log("You lost!");
+    }
+else if (HumanChoice === "scissors" && randomComputerChoice === "computerPaper")
+    {
+    console.log("You won!");
+    }
+else if (HumanChoice === "scissors" && randomComputerChoice === "computerScissors")
+    {
+    console.log("It's a tie!");
+    }
